bdd2ab24b9c25f8875881947cec78fd2e95ef39f
Federico Zipoli <fzi@zurich.ibm.com>
2024-06-19 13:01:28 -0400

    wip: adding entry points
    Signed-off-by: Federico Zipoli <fzi@zurich.ibm.com>

--- service/routes.py
+++ service/routes.py
@@ -4,7 +4,7 @@ Account Service
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
-from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
@@ -57,39 +57,96 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-# ... place you code here to LIST accounts ...
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """Returns a list of accounts"""
+    app.logger.info("Request to list accounts...")
+    accounts = []
+    app.logger.info("Find all")
+    accounts = Account.all()
+    if len(accounts) > 1:
+        results = [account.serialize() for account in accounts]
+    else:
+        results = []
+    app.logger.info("[%s] accounts returned", len(results))
+    return jsonify(results), status.HTTP_200_OK
 
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_account(account_id):
+    """Retrieve a single Account
+    This endpoint will return an account based on it's id
+    """
+    app.logger.info(
+        "Request to Retrieve an account with id [%s]",
+        account_id,
+    )
+    account = Account.find(account_id)
+    if not account:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id '{account_id}' was not found.",
+        )
 
-# ... place you code here to READ an account ...
+    app.logger.info("Returning account: %s", account.name)
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based the body that is posted
+    """
+    app.logger.info(
+        "Request to Update an account with id [%s]",
+        account_id,
+    )
+    check_content_type("application/json")
+    account = Account.find(account_id)
+    if not account:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id '{account_id}' was not found.",
+        )
+    account.deserialize(request.get_json())
+    account.id = account_id
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an account
+    This endpoint will delete an account based the id specified in the path
+    """
+    app.logger.info(
+        "Request to delete an account with id [%s]",
+        account_id,
+    )
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
